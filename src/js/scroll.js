c510037a4744805ac5fa5d0258d00b9868cb9cc9
terrork4r <terrork4r@gmail.com>
2019-08-06 23:20:26 +0300

    исправил скролл и адаптив

--- src/js/scroll.js
+++ src/js/scroll.js
@@ -1,6 +1,17 @@
-$(function(){
-    $("#up").bind('click', function(e){
-        e.preventDefault();
-        $('body,html').animate({scrollTop: 0}, 600);    
+$(document).ready(function () {
+    var scrollup = $('#scrollup');
+    /* скрипт для стрелки вверх */
+    $(window).scroll(function () {
+     if ($(this).scrollTop() > 1) {
+      scrollup.addClass('scrollup_active')
+     } else {
+      scrollup.removeClass('scrollup_active')
+    }
     });
-});
+    scrollup.click(function () {
+     $("html, body").animate({
+      scrollTop: 0
+     }, 500);
+     return false
+    });
+   });
