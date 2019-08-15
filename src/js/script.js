ec456553a3bc5d352f5b29409924fae76d4cfa09
terrork4r <terrork4r@gmail.com>
2019-08-15 23:54:22 +0300

    Сделал валидацию форм

--- src/js/script.js
+++ src/js/script.js
@@ -11,8 +11,73 @@ $('#offer-form').on('submit', function(event) {
       }
   });
 });
-/*Слайдер */
+
 $(document).ready(function(){
+  /*валидация форм */
+  $('#brif-form').validate({
+    rules: {
+      username:{
+        required: true,
+        minlength: 2,
+        maxlength: 15
+      }, 
+      phone: "required",
+      email:{
+        required: true,
+        email: true
+      } 
+    },
+    messages: {
+      username:{
+        required: "Укажите ваше имя",
+        minlength: jQuery.validator.format("Осталось символов {0}!"),
+        maxlength: jQuery.validator.format("Превышен лимит в {0} символов!")
+      }, 
+      phone:"Укажите ваш номер телефона",
+      email: "Укажите ваш email"
+    }
+  });
+
+  $('#offer-form').validate({
+    rules: {
+      username:{
+        required: true,
+        minlength: 2,
+        maxlength: 15
+      }, 
+      userphone: "required"
+    },
+    messages: {
+      username:{
+        required: "Укажите ваше имя",
+        minlength: jQuery.validator.format("Осталось символов {0}!"),
+        maxlength: jQuery.validator.format("Превышен лимит в {0} символов!")
+      }, 
+      userphone:"Укажите ваш номер телефона"
+    }
+  });
+
+  $('#modal-form').validate({
+    rules: {
+      username:{
+        required: true,
+        minlength: 2,
+        maxlength: 15
+      }, 
+      userphone: "required"
+    },
+    messages: {
+      username:{
+        required: "Укажите ваше имя",
+        minlength: jQuery.validator.format("Осталось символов {0}!"),
+        maxlength: jQuery.validator.format("Превышен лимит в {0} символов!")
+      }, 
+      userphone:"Укажите ваш номер телефона"
+    }
+  });
+  /*Маска для телефона */
+  $('.phone').mask('+7(999) 999-99-99')
+  /*Слайдер */
   $('.slider').slick({
     slidesToShow: 3,
     slidesToScroll: 1,
