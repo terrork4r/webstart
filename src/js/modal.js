fcfd8945d56439daf93f5f6b63bf99ba85b38418
terrork4r <terrork4r@gmail.com>
2019-08-16 02:09:20 +0300

    сделал модальное окно

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,14 +1,22 @@
 $(document).ready(function(){
     var button = $('#button');
     var modal = $('#modal');
-    var close = $('#close')
+    var close = $('#close');
+    var cardLink = $('.card__link')
 
     button.on('click', function(){
         modal.addClass('modal_active');
-    })
+    });
+
+    cardLink.on('click', function(event){
+        event.preventDefault();
+        modal.addClass('modal_active');
+        var target = $(this).attr('data-target');
+        $('.modal-content').load(target+".html");
+    });
 
     close.on('click', function(){
         modal.removeClass('modal_active')
-    })
+    });
    
 });
